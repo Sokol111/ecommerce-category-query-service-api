9060f70676e6a66cd8092fe465ef99c9170114f9
Sokol111 <igorsokol111@gmail.com>
2026-03-13 21:32:45 +0100

    u

--- gen/typescript/api.schemas.ts
+++ gen/typescript/api.schemas.ts
@@ -3,7 +3,7 @@
  * Do not edit manually.
  * Category API
  * API for querying categories
- * OpenAPI spec version: 1.1.0
+ * OpenAPI spec version: 1.1.1
  */
 /**
  * How an attribute is used in a category
